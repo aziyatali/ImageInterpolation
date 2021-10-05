1ade97d5726289d842e32b5be485eb0802656e0c
Aziyat <aziyatalik4@gmail.com>
2021-10-06 08:49:11 +0900

    modified cancatting for task4

--- task4.m
+++ task4.m
@@ -11,8 +11,8 @@ G=imcrop(defaultImg,[1, 1*col+1, n, col-2]);
 R=imcrop(defaultImg,[1, 2*col+1, n, col]);
 
 % concat image by putting them back 
-rgbImg = cat(3, R, G, B);
-%ColorImg(:,:,1) = R;
-%ColorImg(:,:,2) = G;
-%ColorImg(:,:,3) = B;
+%rgbImg = cat(3, R, G, B);
+rgbImg(:,:,1) = R;
+rgbImg(:,:,2) = G;
+rgbImg(:,:,3) = B;
 imshow(rgbImg);
